8164ba88aef0157cc9d4c8dcea5cb48e6857e9b6
Zach Gentner <zach.gentner@gmail.com>
2023-11-17 18:18:30 -0500

    init

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@email.com',
+        status: 'active',
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bwilliams@email.com',
+        status: 'inactive',
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'sjackson@email.com',
+        status: 'active',
+    },
+];
+
+module.exports = members;
---
+++ app.js
@@ -0,0 +1,31 @@
+// Import Express.js
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger');
+
+// Initialize an Express.js variable
+const app = express();
+
+// Initializes the logger middleware
+// app.use(logger);
+
+// Body Parses Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// When running server side, rather than on port 5000, it will be in an env variable.
+const PORT = process.env.PORT || 5555;
+
+// Listen on a port
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+// Serve an html file with .sendFile(); Not ideal.
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// Set a static folder with .use() to include middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Require the Members API routes
+app.use('/api/members', require('./routes/api/members'));
